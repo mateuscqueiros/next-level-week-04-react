c74e9f90c307c190b0931db76ca68393c96ebe77
Mateus Queirós <mateuscqueiros1@gmail.com>
2021-02-22 16:37:07 -0300

    first commit

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,14 @@
+export function ExperienceBar() {
+    return (
+        <header className='experience-bar'>
+            <span>8 XP</span>
+            <div>
+                <div style={{ width: '50%' }} />
+                <span className="current-experience" style={{ left: '50%' }}>
+                    300 XP
+                </span>
+            </div>
+            <span>688 xp</span>
+        </header>
+    )
+}
